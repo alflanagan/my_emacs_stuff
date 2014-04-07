738d71ee46aab84e1815cb74bcdaa2c16b50fb86
alflanagan <a.lloyd.flanagan@gmail.com>
2014-04-06 21:04:46 -0400

    sort package list; don't require cl-lib

--- sync_packages.el
+++ sync_packages.el
@@ -1,30 +1,29 @@
-(require 'cl)
 
 ;; Guarantee all packages are installed on start
 (defvar packages-list
-  '(autopair            ;;Automagically pair braces and quotes like TextMate
-    zenburn-theme       ;;A low contrast color theme for Emacs.
-    flycheck            ;;On-the-fly syntax checking (Flymake done right)
-    fic-mode            ;;Show FIXME/TODO/BUG/KLUDGE in special face only in comments and strings
-    epl                 ;;Emacs Package Library
-    ecb                 ;;a code browser for Emacs
-    ;charmap             ;;Unicode table for Emacs -- sadly quite buggy
+ '( autopair            ;;Automagically pair braces and quotes like TextMate
     bash-completion 	;;BASH completion for the shell buffer
+    ;charmap             ;;Unicode table for Emacs -- sadly quite buggy
     dash                ;;A modern list library for Emacs
     discover            ;;discover more of Emacs
-    python-environment  ;;virtualenv API for Emacs Lisp
-    jedi                ;;Python auto-completion for Emacs
+    ecb                 ;;a code browser for Emacs
+    epl                 ;;Emacs Package Library
+    fic-mode            ;;Show FIXME/TODO/BUG/KLUDGE in special face only in comments and strings
+    flycheck            ;;On-the-fly syntax checking (Flymake done right)
     git-commit-mode     ;;Major mode for editing git commit messages
     git-rebase-mode     ;;mode for editing git rebase files
     icicles             ;;Minibuffer input completion and cycling.
+    jedi                ;;Python auto-completion for Emacs
     magit               ;;Git from Emacs
+    markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
     pep8                ;;run the python pep8 checker putting hits in a grep buffer
     ;pylint              ;;minor mode for running `pylint' (superseded  by flycheck)
-    rainbow-mode          ;;Colorize color names in buffers
+    python-environment  ;;virtualenv API for Emacs Lisp
     rainbow-delimiters  ;;Highlight nested parens, brackets, braces a different color at each depth.
+    rainbow-mode          ;;Colorize color names in buffers
     slime               ;;Superior Lisp Interaction Mode for Emacs
     slime-repl          ;;Read-Eval-Print Loop written in Emacs Lisp
-    markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
+    zenburn-theme       ;;A low contrast color theme for Emacs.
 )
   "List of packages to verify at launch")
 
