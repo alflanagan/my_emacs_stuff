7e916db32b129ed519f3559d5404c9e22d5833a1
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-04-14 11:49:12 -0400

    removed arduino-mode, added some required packages, fixed has-package-not-installed

--- sync_packages.el
+++ sync_packages.el
@@ -9,28 +9,30 @@
 ;; Guarantee all packages are installed on start
 (defvar packages-list
   '(
-    arduino-mode          ;;Major mode for the Arduino language
+    arduino-mode        ;;Major mode for the Arduino language
     autopair            ;;Automagically pair braces and quotes like TextMate
     bash-completion 	;;BASH completion for the shell buffer
-    ;charmap             ;;Unicode table for Emacs -- sadly quite buggy
+    ;charmap            ;;Unicode table for Emacs -- sadly quite buggy
     cython-mode         ;;Major mode for editing Cython files
     dash                ;;A modern list library for Emacs
     discover            ;;discover more of Emacs
     ecb                 ;;a code browser for Emacs
-    elpy                  ;;Emacs Python Development Environment
+    fuzzy               ;;dependency of elpy
+    elpy                ;;Emacs Python Development Environment
     epl                 ;;Emacs Package Library
-    ;fic-mode            ;;Show FIXME/TODO/BUG/KLUDGE... (not working; why?)
+    ;fic-mode           ;;Show FIXME/TODO/BUG/KLUDGE... (not working; why?)
     fic-ext-mode        ;;Show FIXME/TODO/BUG/KLUDGE... in special face only in comments and strings
     flycheck            ;;On-the-fly syntax checking (Flymake done right)
     git-commit-mode     ;;Major mode for editing git commit messages
     git-rebase-mode     ;;mode for editing git rebase files
     goto-chg            ;;goto last change
     icicles             ;;Minibuffer input completion and cycling.
+    epc                 ;;dependency of jedi
     jedi                ;;Python auto-completion for Emacs
     magit               ;;Git from Emacs
     markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
     pep8                ;;run the python pep8 checker putting hits in a grep buffer
-    ;pylint              ;;minor mode for running `pylint' (superseded  by flycheck)
+    ;pylint             ;;minor mode for running `pylint' (superseded  by flycheck)
     python-environment  ;;virtualenv API for Emacs Lisp
     rainbow-delimiters  ;;Highlight nested parens, brackets, braces a different color at each depth.
     rainbow-mode        ;;Colorize color names in buffers
@@ -42,7 +44,7 @@
 (defun has-package-not-installed ()
   "Define a version of has-package-not-installed which does not use ugly cl macros."
   ;;except reduce, that is. haven't found a replacement.
-  (cl-reduce (lambda (a b) (and a b))  (mapcar 'package-installed-p packages-list))
+  (not (cl-reduce (lambda (a b) (and a b))  (mapcar 'package-installed-p packages-list)))
 )
 
 (when (has-package-not-installed)
