6f055c4e90ac96bbfffc81a24c17e463273f262f
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-04-16 18:14:47 -0400

    Commented out a lot of packages; will see what's still needed after adding starter-kit

--- sync_packages.el
+++ sync_packages.el
@@ -10,41 +10,33 @@
 ;; Guarantee all packages are installed on start
 (defvar sync-packages-list
   '(
-    arduino-mode        ;;Major mode for the Arduino language
-    autopair            ;;Automagically pair braces and quotes like TextMate
-    bash-completion 	;;BASH completion for the shell buffer
-    ;charmap            ;;Unicode table for Emacs -- sadly quite buggy
-    cython-mode         ;;Major mode for editing Cython files
-    dash                ;;A modern list library for Emacs
-    discover            ;;discover more of Emacs
-    ecb                 ;;a code browser for Emacs
-    fuzzy               ;;dependency of elpy
-    elpy                ;;Emacs Python Development Environment
-    epl                 ;;Emacs Package Library
-    ;fic-mode           ;;Show FIXME/TODO/BUG/KLUDGE... (not working; why?)
-    fic-ext-mode        ;;Show FIXME/TODO/BUG/KLUDGE... in special face only in comments and strings
+    ;; autopair            ;;Automagically pair braces and quotes like TextMate
+    ;; bash-completion     ;;BASH completion for the shell buffer
+    ;; cython-mode         ;;Major mode for editing Cython files
+    ;; dash                ;;A modern list library for Emacs
+    ;; discover            ;;discover more of Emacs
+    ;; ecb                 ;;a code browser for Emacs
+    ;; fuzzy               ;;dependency of elpy
+    ;; elpy                ;;Emacs Python Development Environment
+    ;; epl                 ;;Emacs Package Library
+    ;; fic-ext-mode        ;;Show FIXME/TODO/BUG/KLUDGE... in special face only in comments and strings
     flycheck            ;;On-the-fly syntax checking (Flymake done right)
-    git-commit-mode     ;;Major mode for editing git commit messages
-    git-rebase-mode     ;;mode for editing git rebase files
-    goto-chg            ;;goto last change
-    icicles             ;;Minibuffer input completion and cycling.
-    epc                 ;;dependency of jedi
-    jedi                ;;Python auto-completion for Emacs
-    magit               ;;Git from Emacs
-    markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
+    ;; goto-chg            ;;goto last change
+    ;; icicles             ;;Minibuffer input completion and cycling.
+    ;; epc                 ;;dependency of jedi
+    ;; jedi                ;;Python auto-completion for Emacs
+    ;; markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
     paradox             ;;Display Package Ratings on the *Packages* buffer.
-    pep8                ;;run the python pep8 checker putting hits in a grep buffer
-    ;pylint             ;;minor mode for running `pylint' (superseded  by flycheck)
-    python-environment  ;;virtualenv API for Emacs Lisp
-    rainbow-delimiters  ;;Highlight nested parens, brackets, braces a different color at each depth.
-    rainbow-mode        ;;Colorize color names in buffers
+    ;; pep8                ;;run the python pep8 checker putting hits in a grep buffer
+    ;; python-environment  ;;virtualenv API for Emacs Lisp
+    ;; rainbow-delimiters  ;;Highlight nested parens, brackets, braces a different color at each depth.
+    ;; rainbow-mode        ;;Colorize color names in buffers
     starter-kit           ;;Saner defaults and goodies.
     starter-kit-bindings ;;Saner defaults and goodies: bindings
     starter-kit-eshell  ;; Saner defaults and goodies: eshell tweaks
     starter-kit-js        ;;Saner defaults and goodies for Javascript
     starter-kit-lisp      ;;Saner defaults and goodies for lisp languages
     starter-kit-ruby      ;;Saner defaults and goodies for Ruby
-    zenburn-theme       ;;A low contrast color theme for Emacs.
 )
   "List of packages to verify at launch.")
 
@@ -56,7 +48,7 @@
     (and (car a_list) (reduce-and (cdr a_list)))
     )
   )
-			    
+
 (defun has-package-not-installed ()
   "Returns t if any package in sync-packages-list is not actually installed"
   (not (reduce-and (mapcar 'package-installed-p sync-packages-list)))
