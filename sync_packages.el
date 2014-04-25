ff2c7829cc6a2fb6be94855d28ff7a0b3793d9d5
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-04-25 15:14:40 -0400

    Sort package names, format, remove duplicate lines.

--- sync_packages.el
+++ sync_packages.el
@@ -1,5 +1,5 @@
 ;;; sync-packages.el --- Ensure a set list of packages are installed on emacs startup.
-;; -*- fill-column: 90; -*-
+;; -*- fill-column: 120; -*-
 ;;; Commentary:
 
 ;;; Code:
@@ -11,46 +11,40 @@
 ;; NOTE: this duplicates functionality of el-get, may switch
 (defvar sync-packages-list
   '(
-    ;; autopair            ;;Automagically pair braces and quotes like TextMate
-    ;; bash-completion     ;;BASH completion for the shell buffer
-    ;; cython-mode         ;;Major mode for editing Cython files
-    dash                ;;A modern list library for Emacs
+    ;; autopair               ;;Automagically pair braces and quotes like TextMate
+    ;; bash-completion        ;;BASH completion for the shell buffer
+    ;; cython-mode            ;;Major mode for editing Cython files
+    ;; discover               ;;discover more of Emacs
+    ;; ecb                    ;;a code browser for Emacs
+    ;; elpy                   ;;Emacs Python Development Environment
+    ;; epl                    ;;Emacs Package Library
+    ;; fuzzy                  ;;dependency of elpy
+    ;; goto-chg               ;;goto last change
+    ;; icicles                ;;Minibuffer input completion and cycling.
+    ;; jedi                   ;;Python auto-completion for Emacs
+    ;; pep8                   ;;run the python pep8 checker putting hits in a grep buffer
+    ;; python-environment     ;;virtualenv API for Emacs Lisp
+    ;; rainbow-mode           ;;Colorize color names in buffers
+    ;; ternjs looks like it would be great, but setup is ... problematic (http://ternjs.net)
+    ;; tern                   ;;Tern-powered JavaScript integration
+    ;; tern-auto-complete     ;;Tern Completion by auto-complete.el
+    dash                   ;;A modern list library for Emacs
     dash-functional        ;;Collection of useful combinators for Emacs Lisp
-    ;; discover            ;;discover more of Emacs
-    ;; ecb                 ;;a code browser for Emacs
-    ;; fuzzy               ;;dependency of elpy
-    ;; elpy                ;;Emacs Python Development Environment
-    el-get                  ;;Manage the external elisp bits and pieces you depend upon
-    ;; epc                 ;;dependency of jedi
-    ;; epl                 ;;Emacs Package Library
+    el-get                 ;;Manage the external elisp bits and pieces you depend upon
     flycheck               ;;On-the-fly syntax checking (Flymake done right)
-    ;; goto-chg            ;;goto last change
-    ;; icicles             ;;Minibuffer input completion and cycling.
-    ;; jedi                ;;Python auto-completion for Emacs
-    know-your-http-well    ;;Look up the meaning of HTTP headers, methods, relations,
-    ;;status codes
+    know-your-http-well    ;;Look up the meaning of HTTP headers, methods, relations, status codes
+    markdown-mode          ;;Emacs Major mode for Markdown-formatted text files
     paradox                ;;Display Package Ratings on the *Packages* buffer.
-    markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
-    paradox             ;;Display Package Ratings on the *Packages* buffer.
-    ;; pep8                ;;run the python pep8 checker putting hits in a grep buffer
-    popup               ;;Visual Popup User Interface
-    ;; python-environment  ;;virtualenv API for Emacs Lisp
-    rainbow-delimiters     ;;Highlight nested parens, brackets, braces a different color
-    ;;at each depth.
-    ;; rainbow-mode        ;;Colorize color names in buffers
+    popup                  ;;Visual Popup User Interface
+    rainbow-delimiters     ;;Highlight nested parens, brackets, braces a different color at each depth.
     starter-kit            ;;Saner defaults and goodies.
     starter-kit-bindings   ;;Saner defaults and goodies: bindings
     starter-kit-eshell     ;;Saner defaults and goodies: eshell tweaks
     starter-kit-js         ;;Saner defaults and goodies for Javascript
     starter-kit-lisp       ;;Saner defaults and goodies for lisp languages
     starter-kit-ruby       ;;Saner defaults and goodies for Ruby
-    ;;ternjs looks like it would be great, but setup is ...
-    ;;problematic (http://ternjs.net)
-    ;;tern-auto-complete     ;;Tern Completion by auto-complete.el
-    ;;tern                   ;;Tern-powered JavaScript integration
     web-mode               ;;major mode for editing html templates
     yasnippet              ;;Yet another snippet extension for Emacs.
-
 )
   "List of packages to verify at launch.")
 
