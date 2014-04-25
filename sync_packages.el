3232a87be916a004f9774f7814a542ef61a676c1
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-04-25 15:05:48 -0400

    Added el-get, know-your-http-well, web-mode, and yasnippet

--- sync_packages.el
+++ sync_packages.el
@@ -1,5 +1,5 @@
 ;;; sync-packages.el --- Ensure a set list of packages are installed on emacs startup.
-
+;; -*- fill-column: 90; -*-
 ;;; Commentary:
 
 ;;; Code:
@@ -8,6 +8,7 @@
 (package-initialize)
 
 ;; Guarantee all packages are installed on start
+;; NOTE: this duplicates functionality of el-get, may switch
 (defvar sync-packages-list
   '(
     ;; autopair            ;;Automagically pair braces and quotes like TextMate
@@ -18,29 +19,41 @@
     ;; ecb                 ;;a code browser for Emacs
     ;; fuzzy               ;;dependency of elpy
     ;; elpy                ;;Emacs Python Development Environment
+    el-get                  ;;Manage the external elisp bits and pieces you depend upon
+    ;; epc                 ;;dependency of jedi
     ;; epl                 ;;Emacs Package Library
-    flycheck            ;;On-the-fly syntax checking (Flymake done right)
+    flycheck               ;;On-the-fly syntax checking (Flymake done right)
     ;; goto-chg            ;;goto last change
     ;; icicles             ;;Minibuffer input completion and cycling.
-    ;; epc                 ;;dependency of jedi
     ;; jedi                ;;Python auto-completion for Emacs
-    markdown-mode       ;;Emacs Major mode for Markdown-formatted text files
-    paradox             ;;Display Package Ratings on the *Packages* buffer.
+    know-your-http-well    ;;Look up the meaning of HTTP headers, methods, relations,
+    ;;status codes
+    markdown-mode          ;;Emacs Major mode for Markdown-formatted text files
+    paradox                ;;Display Package Ratings on the *Packages* buffer.
     ;; pep8                ;;run the python pep8 checker putting hits in a grep buffer
     ;; python-environment  ;;virtualenv API for Emacs Lisp
-    rainbow-delimiters  ;;Highlight nested parens, brackets, braces a different color at each depth.
+    rainbow-delimiters     ;;Highlight nested parens, brackets, braces a different color
+    ;;at each depth.
     ;; rainbow-mode        ;;Colorize color names in buffers
-    starter-kit           ;;Saner defaults and goodies.
-    starter-kit-bindings ;;Saner defaults and goodies: bindings
-    starter-kit-eshell  ;; Saner defaults and goodies: eshell tweaks
-    starter-kit-js        ;;Saner defaults and goodies for Javascript
-    starter-kit-lisp      ;;Saner defaults and goodies for lisp languages
-    starter-kit-ruby      ;;Saner defaults and goodies for Ruby
+    starter-kit            ;;Saner defaults and goodies.
+    starter-kit-bindings   ;;Saner defaults and goodies: bindings
+    starter-kit-eshell     ;;Saner defaults and goodies: eshell tweaks
+    starter-kit-js         ;;Saner defaults and goodies for Javascript
+    starter-kit-lisp       ;;Saner defaults and goodies for lisp languages
+    starter-kit-ruby       ;;Saner defaults and goodies for Ruby
+    ;;ternjs looks like it would be great, but setup is ...
+    ;;problematic (http://ternjs.net)
+    ;;tern-auto-complete     ;;Tern Completion by auto-complete.el
+    ;;tern                   ;;Tern-powered JavaScript integration
+    web-mode               ;;major mode for editing html templates
+    yasnippet              ;;Yet another snippet extension for Emacs.
+
 )
   "List of packages to verify at launch.")
 
 (defun reduce-and (a_list)
-  "Reduce a list using and -- i.e. return nil if any element is nil, else return last element"
+  "Reduce a list using and -- i.e. return nil if any element is nil, else return
+last element"
   ;;only needed because you can't (apply 'and (...))
   (if (null a_list)
       t
