478b2ff85476e61227e1ee1ab975fec9b09d44b3
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-05-06 18:15:35 -0400

    Fixed output routines, added test comments

--- experiments.el
+++ experiments.el
@@ -1,11 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
 ;;; for now, just a set of experimental stuff
-(defun parse-bookmark-file (file-name) nil)
 (require 'json)
 (require 'time-date)
 (require 'cl-lib)  ;;OK, maybe CL isn't all bad
-
 
 (defconst test-bookmarks-file "bookmarks-2014-04-25.json"
   "A Firefox JSON export file for testing purposes")
@@ -13,42 +11,85 @@
 (setq json-object (json-read-file test-bookmarks-file))
 
 ;;clean up previous output, if any
-(with-current-buffer (get-buffer-create "*experiments*") (delete-region (point-min) (point-max)) )
+(with-current-buffer (get-buffer-create "*experiments*")
+  (delete-region (point-min) (point-max)) )
 
 (defun convert-moz-time (time-value)
   "Convert mozilla time value to standard emacs (HIGH LOW USEC PSEC)"
   (seconds-to-time (/ time-value 1000000.0)))
+
+;;(convert-moz-time  100000000000001.0) ==> (1525 57600 0 998378)
+;;(convert-moz-time  100000000000001) ==> (1525 57600 0 998378)
+;;(convert-moz-time 0.0) ==> (0 0 0 0)
 
 (defun format-moz-time-iso-8601 (time-value)
   "Format time-value from mozilla JSON to ISO 8601 standard format, return as string"
   (format-time-string "%FT%T%z"  (convert-moz-time time-value))
   )
 
+;;(format-moz-time-iso-8601 0.0) ==> "1969-12-31T19:00:00-0500"
+;;(format-moz-time-iso-8601 (* (float-time) 1000000.0)) ==> "2014-05-06T17:57:43-0400" (YMMV)
+
+(defun fuzzy-float-string (any-float)
+  "Format a float as a date string if it would result in a
+reasonable value, as float otherwise"
+  ;;assume floating-point value is date if would convert to
+  ;;later than 3/3/1973
+  (if (and (> any-float  1.0e14)
+           (< any-float 2.14745e15))
+      (format-moz-time-iso-8601 any-float)
+    (number-to-string any-float)))
+
+;;(fuzzy-float-string 7.0) ==> "7.0"
+;;(fuzzy-float-string  100000000000001.0) ==> "1973-03-03T04:46:40-0500"
+;;(fuzzy-float-string  2147449999999999.0) ==> "2038-01-18T12:53:19-0500"
+;;(fuzzy-float-string  2147450000000000.0) ==> "2.14745e+15"
+
 (defun make-string (any-value)
   "Format any-value into a string. (Surely there's a standard function for this?)"
-  (cond ((eql (type-of any-value) "string") any-value)
-        ;;assume floating-point value is date if would convert to
-        ;;later than 3/3/1973
-        ((eql (type-of any-value) "float") (cond (and ((>  100000000000000.0 any-value))
-                                                      ((<  2147450000000000.0 any-value)))
-                                                 (format-moz-time-iso-8601  any-value)
-                                                 (number-to-string any-value)))
-        ((eql (type-of any-value) "integer") (number-to-string any-value)))
+  (cond ((equal (type-of any-value) 'string)
+          any-value)
+        ((equal (type-of any-value) 'integer)
+         (number-to-string any-value))
+        ((equal (type-of any-value) 'float)
+         (fuzzy-float-string any-value))
+        ('t
+         "unknown type")
+        )
   )
 
-(defun make-float-date (any-value)
-  (cond (and (>  100000000000000.0 any-value)
-             (<  2147450000000000.0 any-value))
-        (format-moz-time-iso-8601  any-value)
-        (t any-value)))
+;;(make-string "title") ==> "title"
+;;(make-string 7) ==> "7"
+;;(make-string 7.0) ==> "7.0"
+;;(make-string 1137450000000000.0) ==> "2006-01-16T17:20:00-0500"
+;;(make-string '(1 2)) ==> "unknown type"
+
+(defun my-output-string (a-string)
+  "Writes a single string or character to the output-buffer *experiments*"
+  (let ((outputbuffer (get-buffer-create "*experiments*")))
+    (with-current-buffer outputbuffer
+      (goto-char (point-max))
+      (insert a-string)
+      )
+    )
+  )
+
+;;(my-output-string "fred") --> |fred
+;;(my-output-string 7) --> |^G
+
+(defun my-output-strings (&rest output-strings)
+  "Write a list of strings to the output buffer *experiments*"
+  (mapc 'my-output-string output-strings)
+  )
+
+;;(my-output-strings "fred" "barney" "wilma") --> fredbarneywilma
 
 (defun my-output (&rest output-values)
   "Output text  by writing to my custom buffer"
   (setq outbuffer  (get-buffer-create "*experiments*"))
   (let ((output-strings (mapcar 'make-string output-values)))
     (setq output-strings (cl-remove-if 'null output-strings))
-    (princ (type-of output-strings))
-    (with-current-buffer outbuffer (goto-char (point-max)) (mapcar 'insert output-strings) (insert "\n"))))
+    (with-current-buffer outbuffer (goto-char (point-max)) (mapc 'insert output-strings) (insert "\n"))))
 
 (defun handle-bookmark (bookmark)
   ;;bookmark is a dotted pair
@@ -63,9 +104,5 @@
     (`children (my-output "children " (number-to-string (length (cdr bookmark))))))
   nil)
 
-(mapcar 'handle-bookmark json-object)
-
-;;(mapcar 'handle-bookmark '((fred . wilma) (title . "gone with the")))
-
-
+(mapc 'handle-bookmark json-object)
 
