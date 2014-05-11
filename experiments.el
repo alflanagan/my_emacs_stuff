2a59a9528d6514f5edecc9804e580b54413f019b
alflanagan <a.lloyd.flanagan@gmail.com>
2014-05-11 09:07:41 -0400

    Updated global names with namespace prefix

--- experiments.el
+++ experiments.el
@@ -1,70 +1,71 @@
 ;; -*- lexical-binding: t -*-
+;; namespace prefix for this file: lbkmk-
 
 ;;; for now, just a set of experimental stuff
-(require 'json)
-(require 'time-date)
-(require 'cl-lib)  ;;OK, maybe CL isn't all bad
+(eval-when-compile (require 'json))
+(eval-when-compile (require 'time-date))
+(eval-when-compile (require 'cl-lib)) ;;OK, maybe CL isn't all bad
 
-(defconst test-bookmarks-file "bookmarks-2014-04-25.json"
+(defconst lbkmk-test-bookmarks-file "bookmarks-2014-04-25.json"
   "A Firefox JSON export file for testing purposes")
 
-(setq json-object (json-read-file test-bookmarks-file))
+(setq lbkmk-json-object (json-read-file lbkmk-test-bookmarks-file))
 
 ;;clean up previous output, if any
 (with-current-buffer (get-buffer-create "*experiments*")
   (delete-region (point-min) (point-max)) )
 
-(defun convert-moz-time (time-value)
+(defun lbkmk-convert-moz-time (time-value)
   "Convert mozilla time value to standard emacs (HIGH LOW USEC PSEC)"
   (seconds-to-time (/ time-value 1000000.0)))
 
-;;(convert-moz-time  100000000000001.0) ==> (1525 57600 0 998378)
-;;(convert-moz-time  100000000000001) ==> (1525 57600 0 998378)
-;;(convert-moz-time 0.0) ==> (0 0 0 0)
+;;(lbkmk-convert-moz-time  100000000000001.0) ==> (1525 57600 0 998378)
+;;(lbkmk-convert-moz-time  100000000000001) ==> (1525 57600 0 998378)
+;;(lbkmk-convert-moz-time 0.0) ==> (0 0 0 0)
 
-(defun format-moz-time-iso-8601 (time-value)
+(defun lbkmk-format-moz-time-iso-8601 (time-value)
   "Format time-value from mozilla JSON to ISO 8601 standard format, return as string"
-  (format-time-string "%FT%T%z"  (convert-moz-time time-value))
+  (format-time-string "%FT%T%z"  (lbkmk-convert-moz-time time-value))
   )
 
-;;(format-moz-time-iso-8601 0.0) ==> "1969-12-31T19:00:00-0500"
-;;(format-moz-time-iso-8601 (* (float-time) 1000000.0)) ==> "2014-05-06T17:57:43-0400" (YMMV)
+;;(lbkmk-format-moz-time-iso-8601 0.0) ==> "1969-12-31T19:00:00-0500"
+;;(lbkmk-format-moz-time-iso-8601 (* (float-time) 1000000.0)) ==> "2014-05-06T17:57:43-0400" (YMMV)
 
-(defun fuzzy-float-string (any-float)
+(defun lbkmk-fuzzy-float-str (any-float)
   "Format a float as a date string if it would result in a
 reasonable value, as float otherwise"
   ;;assume floating-point value is date if would convert to
   ;;later than 3/3/1973
   (if (and (> any-float  1.0e14)
            (< any-float 2.14745e15))
-      (format-moz-time-iso-8601 any-float)
+      (lbkmk-format-moz-time-iso-8601 any-float)
     (number-to-string any-float)))
 
-;;(fuzzy-float-string 7.0) ==> "7.0"
-;;(fuzzy-float-string  100000000000001.0) ==> "1973-03-03T04:46:40-0500"
-;;(fuzzy-float-string  2147449999999999.0) ==> "2038-01-18T12:53:19-0500"
-;;(fuzzy-float-string  2147450000000000.0) ==> "2.14745e+15"
+;;(lbkmk-fuzzy-float-str 7.0) ==> "7.0"
+;;(lbkmk-fuzzy-float-str  100000000000001.0) ==> "1973-03-03T04:46:40-0500"
+;;(lbkmk-fuzzy-float-str  2147449999999999.0) ==> "2038-01-18T12:53:19-0500"
+;;(lbkmk-fuzzy-float-str  2147450000000000.0) ==> "2.14745e+15"
 
-(defun my-make-output-string (any-value)
+(defun lbkmk-make-str (any-value)
   "Format any-value into a string. (Surely there's a standard function for this?)"
   (cond ((equal (type-of any-value) 'string)
           any-value)
         ((equal (type-of any-value) 'integer)
          (number-to-string any-value))
         ((equal (type-of any-value) 'float)
-         (fuzzy-float-string any-value))
+         (lbkmk-fuzzy-float-str any-value))
         ('t
          "unknown type")
         )
   )
 
-;;(my-make-output-string "title") ==> "title"
-;;(my-make-output-string 7) ==> "7"
-;;(my-make-output-string 7.0) ==> "7.0"
-;;(my-make-output-string 1137450000000000.0) ==> "2006-01-16T17:20:00-0500"
-;;(my-make-output-string '(1 2)) ==> "unknown type"
+;;(lbkmk-make-str "title") ==> "title"
+;;(lbkmk-make-str 7) ==> "7"
+;;(lbkmk-make-str 7.0) ==> "7.0"
+;;(lbkmk-make-str 1137450000000000.0) ==> "2006-01-16T17:20:00-0500"
+;;(lbkmk-make-str '(1 2)) ==> "unknown type"
 
-(defun my-output-string (a-string)
+(defun lbkmk-output-str (a-string)
   "Writes a single string or character to the output-buffer *experiments*"
   (let ((outputbuffer (get-buffer-create "*experiments*")))
     (with-current-buffer outputbuffer
@@ -74,10 +75,10 @@ reasonable value, as float otherwise"
     )
   )
 
-;;(my-output-string "fred") --> |fred
-;;(my-output-string 7) --> |^G
+;;(lbkmk-output-str "fred") --> |fred
+;;(lbkmk-output-str 7) --> |^G
 
-(defun my-output-strings (&rest output-strings)
+(defun lbkmk-output-strs (&rest output-strings)
   "Write a list of strings to the output buffer *experiments*"
   (let ((outputbuffer (get-buffer-create "*experiments*")))
     (with-current-buffer outputbuffer
@@ -87,17 +88,17 @@ reasonable value, as float otherwise"
     )
   )
 
-;;(my-output-strings "fred" "barney" "wilma") --> fredbarneywilma
+;;(lbkmk-output-strs "fred" "barney" "wilma") --> fredbarneywilma
 
-(defun my-output
+(defun lbkmk-output
     (&rest output-values)
   "Output text  by writing to my custom buffer"
   (let
       ((output-strings
-        (mapcar 'my-make-output-string output-values)))
+        (mapcar 'lbkmk-make-str output-values)))
     (setq output-strings
           (cl-remove-if 'null output-strings))
-    (apply 'my-output-strings output-strings)))
+    (apply 'lbkmk-output-strs output-strings)))
 
 
 ;;; bookmarklet for displaying bookmark json files in browser:
@@ -120,18 +121,18 @@ reasonable value, as float otherwise"
 ;; )();
 
 
-(defun handle-bookmark (bookmark)
+(defun lbkmk-handle-bookmark (bookmark)
   ;;bookmark is a dotted pair
   (if (not (atom (cdr bookmark))) (error "Argument must be dotted pair, got %s" (type-of bookmark)))
   (pcase (car bookmark)
-    (`title (my-output "Title is " (cdr bookmark) "\n"))
-    (`id  (my-output "ID is " (number-to-string (cdr bookmark)) "\n"))
-    (`dateAdded  (my-output "Date added is " (format-moz-time-iso-8601 (cdr bookmark)) "\n"))
-    (`lastModified (my-output "Date modified is " (format-moz-time-iso-8601 (cdr bookmark)) "\n"))
-    (`root (my-output "root is " (cdr bookmark) "\n"))
-    (`type (my-output "type is " (cdr bookmark) "\n")
+    (`title (lbkmk-output "Title is " (cdr bookmark) "\n"))
+    (`id  (lbkmk-output "ID is " (number-to-string (cdr bookmark)) "\n"))
+    (`dateAdded  (lbkmk-output "Date added is " (lbkmk-format-moz-time-iso-8601 (cdr bookmark)) "\n"))
+    (`lastModified (lbkmk-output "Date modified is " (lbkmk-format-moz-time-iso-8601 (cdr bookmark)) "\n"))
+    (`root (lbkmk-output "root is " (cdr bookmark) "\n"))
+    (`type (lbkmk-output "type is " (cdr bookmark) "\n")
            (if (equal (cdr bookmark) "text/x-moz-place")
-               (my-output "found place")))
+               (lbkmk-output "found place")))
     ;; type serves to distinguish 'object' classes
     ;; Known values:
     ;;  1 (looks like internal directives: read-only?)
@@ -140,29 +141,29 @@ reasonable value, as float otherwise"
     ;; text/x-moz-place-container
     ;; text/x-moz-place-separator
 
-    (`children (my-output "children " (number-to-string (length (cdr bookmark))) "\n")
-               (my-output (pp-to-string (mapcar (lambda (x) (list (car x) (cadr x))) (cdr bookmark))))
+    (`children (lbkmk-output "children " (number-to-string (length (cdr bookmark))) "\n")
+               (lbkmk-output (pp-to-string (mapcar (lambda (x) (list (car x) (cadr x))) (cdr bookmark))))
                ))
   nil)
 
-(mapc 'handle-bookmark json-object)
+(mapc 'lbkmk-handle-bookmark lbkmk-json-object)
 
 
-(cl-defstruct x-moz-place uri type lastModified dateAdded parent id title index)
+(cl-defstruct lbkmk-moz-place uri type lastModified dateAdded parent id title index)
 
-;;(make-x-moz-place) ==> [cl-struct-x-moz-place nil nil nil nil nil nil nil nil]
-;;(make-x-moz-place :uri "http://www.example.com" :type "text" :lastModified 1340392082000000
+;;(make-lbkmk-moz-place) ==> [cl-struct-lbkmk-moz-place nil nil nil nil nil nil nil nil]
+;;(make-lbkmk-moz-place :uri "http://www.example.com" :type "text" :lastModified 1340392082000000
 ;;                   :dateAdded 1340391622000000 :parent 3860 :id 4153 :title "NINA - Devbox"
-;;                   :index 9) ==> [cl-struct-x-moz-place "http://www.example.com" "text"
+;;                   :index 9) ==> [cl-struct-lbkmk-moz-place "http://www.example.com" "text"
 ;;                   1.340392082e+15 1.340391622e+15 3860 4153 "NINA - Devbox" 9]
 
-(setq test-bookmark
-     (make-x-moz-place :uri "http://www.example.com" :type "text" :lastModified 1340392082000000
+(setq lbkmk-test-bookmark
+     (make-lbkmk-moz-place :uri "http://www.example.com" :type "text" :lastModified 1340392082000000
                         :dateAdded 1340391622000000 :parent 3860 :id 4153 :title "NINA - Devbox"
                         :index 9))
-;; (setq test-bookmark2 (copy-x-moz-place test-bookmark))
-;; (x-moz-place-p test-bookmark2) ==> t
-;;(equal test-bookmark test-bookmark2) ==> t
-;;(eq test-bookmark test-bookmark2) ==> nil
-;;(x-moz-place-title test-bookmark) ==> "NINA - Devbox"
-;;(x-moz-place-type test-bookmark) ==> "text"
+;; (setq lbkmk-test-bookmark2 (copy-lbkmk-moz-place lbkmk-test-bookmark))
+;; (lbkmk-moz-place-p lbkmk-test-bookmark2) ==> t
+;;(equal lbkmk-test-bookmark lbkmk-test-bookmark2) ==> t
+;;(eq lbkmk-test-bookmark lbkmk-test-bookmark2) ==> nil
+;;(lbkmk-moz-place-title lbkmk-test-bookmark) ==> "NINA - Devbox"
+;;(lbkmk-moz-place-type lbkmk-test-bookmark) ==> "text"
