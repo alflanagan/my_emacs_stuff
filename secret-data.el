0a98bb9fbeb340db1b5469210c8d55a732092e9e
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-08-18 15:23:21 -0400

    Added custom var secret-data-logging-enabled Implemented: * secret-data-get-key * secret-data-store-value Added functions: * secret-data-log * split-on-equal-if-key * secret-data-write-to-file * replace-file-with-back moved unit tests to test-secret-data.el

--- secret-data.el
+++ secret-data.el
@@ -15,38 +15,88 @@
   :risky t
   )
 
+
+(defcustom secret-data-logging-enabled nil
+  "Whether `secret-data' logging messages are written."
+  :group 'secret-data
+  :type 'boolean
+  :safe t
+  )
+
+
+(defun secret-data-log (msg &rest args)
+  "Writes a message with format from MSG and values ARGS, only if `secret-data-logging-enabled` is t."
+  (when secret-data-logging-enabled
+    (apply 'message msg args)))
+
+
+(defun split-on-equal-if-key (a-string)
+  "Splits A-STRING on first '=' character, return list of two strings.  Return nil if no '=' is present, or if '=' is first character."
+  (let* ((values (split-string a-string "="))
+         (part1 (car values))
+         ;;value may have embedded equal signs; put them back
+         (part2 (string-join (cdr values) "=")))
+    (if (or (string-equal part1 "")
+            (< (length values) 2))
+        nil
+      (list part1 part2 ))))
+
+
 (defun secret-data-parse ()
   "Parse contents of current buffer, which should contain lines of the form KEY=VALUE, into a standard a-list."
   (mapcar
-   (lambda (x) (list (car (split-string x "="))
-                ;;value may have embedded equal signs; put them back
-                (string-join (cdr (split-string x "=")) "=")))
+   (lambda (x) (split-on-equal-if-key x))
    (split-string (buffer-string) "[\f\n\r]+"))
   )
 
+
 (defun secret-data-get-key (some-key &optional file-name)
-  "Retrieve the value associated with SOME-KEY from the data stored in FILE-NAME.  FILE-NAME defaults to custom variable `secret-data-store-file'."
+  "Retrieve the value associated with SOME-KEY from the data stored in FILE-NAME.  FILE-NAME defaults to the value of custom variable `secret-data-store-file'."
+  (let ((fname (if file-name file-name secret-data-store-file)))
+    (secret-data-log "FILE-NAME is '%s', fname is '%s'." file-name fname)
+    (condition-case err-data
+        (with-temp-buffer
+          (insert-file-contents (expand-file-name fname))
+          (cadr (assoc some-key (secret-data-parse)))
+          )
+      (file-error (message "Can't get secret data with key [%s]: can't open file %s: (%s)."
+                           some-key fname (cl-caddr err-data))))))
+
+
+(defun secret-data-write-data-to-file (a-list file-name)
+  "Writes the contents of A-LIST to file with name FILE-NAME, as key-value pairs."
   (condition-case err-data
-      (with-temp-buffer
-        (insert-file-contents (expand-file-name "~/.emacs-secret"))
-        (cadr (assoc some-key (secret-data-parse)))
-        )
-    (file-error (message "Can't get secret data with key [%s]: can't open file %s: (%s)."
-                         some-key
-                         (expand-file-name secret-data-store-file)
-                         (cl-caddr err-data))))
-  )
+      (with-current-buffer (find-file-literally file-name)
+        (mapc (lambda (x) (when x (insert (format "%s=%s\n" (car x) (cadr x))))) a-list)
+        (save-buffer)
+        (kill-buffer))
+    (file-error (message "Failed writing data to file [%s]: (%s)."
+                         file-name (cl-caddr err-data)))))
 
-(secret-data-get-key "paradox-github-token")
-(secret-data-get-key "pinboard-password")
-(secret-data-get-key "this-is-a-test")
-(secret-data-get-key "test-whitespace")
 
-(defun secret-data-store-value (some-key some-value)
-  "Update the data store so that retrieval of SOME-KEY will provide value SOME-VALUE."
-  
-  "data store goes here")
+(defun replace-file-with-back (source-file replace-file)
+  "Save the contents of the file named SOURCE-FILE to the file name REPLACE-FILE.  If REPLACE-FILE names an existing file, the existing file is renamed with the addition of a backup file extension."
+  (when (file-exists-p replace-file)
+    (rename-file replace-file (concat replace-file ".bkp") t))
+  (rename-file source-file replace-file))
+
+
+(defun secret-data-store-value (some-key some-value &optional file-name)
+  "Assign to SOME-KEY the value SOME-VALUE in the data store in file FILE-NAME.  FILE-NAME defaults to the value of custom variable `secret-data-store-file'."
+  (let ((fname (if file-name file-name secret-data-store-file)))
+    (condition-case err-data
+        (with-temp-buffer
+          (insert-file-contents (expand-file-name fname))
+          (let* ((read-a-list  (secret-data-parse))
+                 (key-value (assoc some-key read-a-list)))
+            (if key-value
+                (setf (cadr (assoc some-key read-a-list)) some-value)
+              (setq read-a-list (append read-a-list `((,some-key ,some-value)))))
+            (let ((new-file-name (make-temp-file "secret-data-store")))
+              (secret-data-write-data-to-file read-a-list new-file-name)
+              (replace-file-with-back new-file-name file-name))))
+      (file-error (message "Failed writing value to key [%s]: can't open file %s: (%s)."
+                           some-key fname (cl-caddr err-data))))))
 
 (provide 'secret-data)
-
 ;;; secret-data.el ends here
--- test-secret-data.el
+++ test-secret-data.el
@@ -7,29 +7,42 @@
 (require 'ert)
 (require 'secret-data)
 
-(let ((temp-file-name  (make-temp-file "secret-data-test"))) temp-file-name)
+
 (defun test-secret-data-fixture (a-function &rest args)
   "Call A-FUNCTION with arguments ARGS and the name of a temporary file containing test data."
-  (let ((temp-file-name  (make-temp-file "secret-data-test")))
+  ;; TODO: this is severely over-complicated -- simplify
+  (let ((temp-file-name  (make-temp-file "secret-data-test"))
+        (return-value nil))
+    (message "TEMP-FILE-NAME is '%s'" temp-file-name)
     (unwind-protect
         (progn (with-temp-file temp-file-name
                  (insert "value1=somerandomstring\npinboard-user-name=a.lloyd.flanagan\nsome-password=aA@#%&*$@#$@#$\\_KRr\nthis-is-a-test=has=equal=signs\ntest-whitespace=this is spaced\tand\ttabbed"))
                (message "created %s." temp-file-name)
-               (apply a-function (append args '(temp-file-name))))
-      (delete-file temp-file-name))))
+               ;; 
+               (mapc (lambda (arg) (message "ARG is %s" arg)) args)
+               (setq return-value (apply a-function (append args (list temp-file-name))))
+               (delete-file temp-file-name)
+               return-value)
+    )))
 
-(test-secret-data-fixture 'split-string "=")
-(ert-deftest test-secret-data-key-retrieval
-  "Tests functionality of secret-data-get-key."
-  (should (equal (secret-data-get-key "value1") "fred"))
-  (should (equal (lbkmk-convert-moz-time  100000000000001) '(1525 57600 0 998378)))
-  (should (equal (lbkmk-convert-moz-time  0.0) '(0 0 0 0)))
-  (should (equal (lbkmk-convert-moz-time nil) nil)))
+(ert-deftest test-secret-data-key-retrieval ()
+    "Tests functionality of secret-data-get-key."
+  
+    (should (equal (test-secret-data-fixture 'secret-data-get-key "value1") "somerandomstring"))
+    (should (equal (test-secret-data-fixture 'secret-data-get-key "pinboard-user-name") "a.lloyd.flanagan"))
+    (should (equal (test-secret-data-fixture 'secret-data-get-key "some-password") "aA@#%&*$@#$@#$\\_KRr")))
 
-(secret-data-get-key "paradox-github-token")
-(secret-data-get-key "pinboard-password")
-(secret-data-get-key "this-is-a-test")
-(secret-data-get-key "test-whitespace")
+(ert-deftest test-secret-embedded-equal-signs ()
+  (should (equal (test-secret-data-fixture 'secret-data-get-key "this-is-a-test") "has=equal=signs")))
+
+(ert-deftest test-secret-embedded-whitespace ()
+  (should (equal (test-secret-data-fixture 'secret-data-get-key "test-whitespace") "this is spaced\tand\ttabbed")))
+
+(ert-deftest test-secret-data-split-on-equal-if-key ()
+  (should (equal (split-on-equal-if-key "") nil))
+  (should (equal (split-on-equal-if-key "=fred") nil))
+  (should (equal (split-on-equal-if-key "fred=") '("fred" "")))
+  (should (equal (split-on-equal-if-key "fred=wilma") '("fred" "wilma"))))
 
 (provide 'test-secret-data)
 ;;; test-secret-data.el ends here
