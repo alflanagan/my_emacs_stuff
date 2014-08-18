5e1972c6b953f589a23f43f134cb9119b1abf479
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-08-18 17:30:08 -0400

    Moved more unit tests to test, added to ert-runner

--- test/my_emacs_stuff-test.el
+++ test/my_emacs_stuff-test.el
@@ -1,13 +1,20 @@
-;;; my_emacs_stuff-test -- master test file for ert-runner -*- lexical-scoping: t -*-
+;;; my_emacs_stuff-test -- master test file for ert-runner -*- lexical-binding: t -*-
 
 ;;; Commentary:
 
 ;;; Code:
-(add-to-list 'load-path ".")
+(add-to-list 'load-path (expand-file-name "."))
+(add-to-list 'load-path (expand-file-name "test"))
+
+;; debug: print everything in load-path not under directories named
+;; '.cask' or 'opt'
+;; (mapc (lambda (x) (when  (not (or (string-match ".*\\.cask.*" x) (string-match ".*/opt/.*" x))) (message x)))
+;;       load-path)
+
 (require 'test-secret-data)
+(require 'test-eieio)
+(require 'test-lib-bookmark)
 (ert t)
 
 (provide 'my_emacs_stuff-test)
 ;;; my_emacs_stuff-test.el ends here
-
-
