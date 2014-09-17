59df0127c92a36eeb829c6a0f2ae044e93954bc7
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-09-17 18:54:41 -0400

    further development. added s module for (s-replace)

--- check-cask.el
+++ check-cask.el
@@ -1,5 +1,4 @@
 ;;; check-cask -- compare loaded packages to contents of Cask file  -*- lexical-binding: t; -*-
-;;; specified
 
 ;;; Commentary:
 
@@ -8,17 +7,26 @@
 
 (require 'package)
 (require 'dash)
+(require 's)
 
 (setq check-cask-installed-package-names (mapcar 'car package-alist))
+(setq check-cask-installed-count (length check-cask-installed-package-names))
+
 
 (defun get-cask-package-names (file-name)
   "Read cask file FILE-NAME and extract the package names from depends functions, return as list."
   (with-temp-buffer (insert-file-contents file-name)
-                    (-filter (lambda (a-str) (string-prefix-p "(depends" a-str))
-                             (split-string (buffer-string) "[\n\r]+"))
-                    ))
+                    (mapcar (lambda (a-line) (s-replace "\")" "" (s-replace "(depends-on \"" "" a-line)))
+                            (-filter (lambda (a-str) (string-prefix-p "(depends" a-str))
+                                     (split-string (buffer-string) "[\n\r]+")))))
 
-(get-cask-package-names "Cask.full")
+(setq check-cask-file-package-names (get-cask-package-names "Cask.full"))
+(length check-cask-file-package-names)
+(setq check-cask-file-package-names (delete-dups check-cask-file-package-names))
+(length check-cask-file-package-names)
+
+(mapcar (lambda (a-line) (s-replace "(depends-on \"" "" a-line))
+        '("(depends-on \"fred\")" "(depends-on \"wilma\")"))
 
 (provide 'check-cask)
 ;;; check-cask.el ends here
